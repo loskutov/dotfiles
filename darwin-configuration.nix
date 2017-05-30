5f47f30bdacc9ca77e8077e237a0a1396c5aad2b
Ignat Loskutov <ignat.loskutov@gmail.com>
2017-05-30 19:17:22 +0300

    Bump darwin-configuration.nix

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -1,24 +1,30 @@
 { config, lib, pkgs, ... }:
 let
-  python27WithPackages = pkgs.python27.withPackages (ps: with ps; [
-    jedi
-    numpy
-    # notebook
-    protobuf3_2
-    # scikitlearn
-    # jupyter
-  ]);
-
   python35WithPackages = pkgs.python35.withPackages (ps: with ps; [
     # numpy
     # jedi
-    jupyter_core
-    jupyter_client
+    # jupyter_core
+    # jupyter_client
     # pandas
     scikitlearn
     notebook
-    matplotlib
+    # matplotlib
   ]);
+  haskellWithPackages = pkgs.haskellPackages.ghcWithPackages (pkgs: with pkgs; [
+    # apply-refact
+    attoparsec
+    base-unicode-symbols
+    hindent
+    hlint
+    stylish-haskell
+  ]);
+  # unfortunately, rust hasn't been updated for a pretty long time in nixpkgs :(
+  rustPackages = with pkgs; [
+    cargo
+    rustc
+    rustfmt
+    rustracer
+  ];
 in {
   # nix.useSandbox = "relaxed";
   # nixpkgs.config.allowBroken = true;
@@ -26,35 +32,41 @@ in {
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
     androidenv.platformTools
+    clang_4
     dos2unix
     emacs
-    gitMinimal
+    gitAndTools.gitFull
     gnupg1compat
+    htop
+    mosh
     mutt
     nix-repl
+    rustup
     # sbt
     # scala
-    python27WithPackages
-    # python35WithPackages
+    tree
+    python35WithPackages
     source-code-pro
-    haskellPackages.idris
-    (haskellPackages.ghcWithPackages (pkgs: with pkgs; [
-      apply-refact
-      attoparsec
-      base-unicode-symbols
-      hindent
-      hlint
-      stylish-haskell
-    ]))
+    # haskellPackages.idris
+    haskellWithPackages
     (texlive.combine { inherit (texlive) beamer scheme-minimal lh lhcyr t2 cyrillic; })
-    # grml-zsh-config
     zsh
   ];
 
   # Create /etc/bashrc that loads the nix-darwin environment.
   # programs.bash.enable = true;
-  programs.zsh.enable = true;
-  programs.zsh.enableSyntaxHighlighting = true;
+  programs.zsh = {
+    enable = true;
+    enableSyntaxHighlighting = true;
+    interactiveShellInit = ''
+      bindkey '^[[A' up-line-or-search
+      bindkey '^[[B' down-line-or-search
+      setopt auto_cd
+      setopt notify
+      export EDITOR=vim
+      REPORTTIME=10
+    '';
+  };
 
   # Recreate /run/current-system symlink after boot.
   services.activate-system.enable = true;
